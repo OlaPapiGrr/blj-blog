4b72d7422cfcc7a1288a67444175f9d69d310a52
BryanOlaPapiGrr <bryan.amrein@gmx.ch>
2021-12-02 15:30:34 +0100

    db change

--- blog_model.php
+++ blog_model.php
@@ -1,9 +1,9 @@
 <?php
-    $user = 'd041e_bramrein';
-    $password = '54321_Db!!!';
+    $user = 'root';
+    $password = '';
     $database = 'post';
     
-    $pdo = new PDO('mysql:host=mysql2.webland.ch;dbname=' . $database, $user, $password, [
+    $pdo = new PDO('mysql:host=localhost;dbname=' . $database, $user, $password, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
